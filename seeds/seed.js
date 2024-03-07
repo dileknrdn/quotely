5ba3b445e8b1069a8a0db09c695b47961dc01d41
Neglon <thomasfneylon@gmail.com>
2024-03-07 12:05:27 -0500

    created base file structure and copied some starter files
    base file structure created, including some starting code

---
+++ seeds/seed.js
@@ -0,0 +1,50 @@
+// seed.js file to execute seeding of databases
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// mini project seed.js file below
+
+// const sequelize = require('../config/connection');
+// const { User, Project } = require('../models');
+
+// const userData = require('./userData.json');
+// const projectData = require('./projectData.json');
+
+// const seedDatabase = async () => {
+//   await sequelize.sync({ force: true });
+
+//   const users = await User.bulkCreate(userData, {
+//     individualHooks: true,
+//     returning: true,
+//   });
+
+//   for (const project of projectData) {
+//     await Project.create({
+//       ...project,
+//       user_id: users[Math.floor(Math.random() * users.length)].id,
+//     });
+//   }
+
+//   process.exit(0);
+// };
+
+// seedDatabase();
