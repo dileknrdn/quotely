837cc1e8f511c1e9391b5c3c1068b1c801cbfc23
Neglon <thomasfneylon@gmail.com>
2024-03-07 21:05:06 -0500

    Update index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const quoteRoutes = require('./quoteRoutes');
 
-router.use('/users', userRoutes);
+router.use('/user', userRoutes);
 router.use('/quotes', quoteRoutes);
 
 module.exports = router;
