4671c88acdd21452da397ea21595e2080c08a8f0
Neglon <thomasfneylon@gmail.com>
2024-03-15 08:53:42 -0400

    buttons populating
    buttons populate on homepage when logged in

---
+++ public/js/addquote.js
@@ -0,0 +1,16 @@
+  // Function to show modal when like button is clicked
+  function likeModal() {
+    var modal = document.getElementById("likeModal");
+    modal.style.display = "block";
+    setTimeout(() => {
+        modal.style.display = "none";
+    }, 1000); // Hide modal after 1 second
+}
+ // Creates the like button dynamically
+ let likeButton = document.createElement("button");
+ //favoriteButton.innerHTML = '<i class="fas fa-heart favorite-btn"></i>';
+ likeButtonButton.classList.add("favorite-btn");
+ likeButton.onclick = () => {
+     addToFavorites(item);
+     likeModal();
+ };
